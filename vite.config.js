04f1b09bd913f67c50402b950c33af280fb80b09
Aeuv Ro <andikario0101@gmail.com>
2026-07-29 14:57:17 +0700

    refactor: migrate from @vitejs/plugin-react to @vitejs/plugin-react-swc

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
+// Using SWC plugin for faster builds and better Vite 8 compatibility
 export default defineConfig({
   plugins: [react()],
   server: {
